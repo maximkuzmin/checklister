e4803063b888a7a9baa74551e420df175880faf6
Max Kuzmin <mail@maximkuzmin.rocks>
2024-02-22 11:58:18 +0300

    dockermagic fixed to work

--- dockermagic.sh
+++ dockermagic.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 COMMIT=$(git rev-parse --verify HEAD)
-docker image build -f “Dockerfile” . \
-  --build-arg “app_name=Checklister” \
-  -t “MyApp:latest” \
-  -t “MyApp:${COMMIT}
+docker image build -f "Dockerfile" . \
+  -t "maximkuzmin/checklister:latest" \
+  -t "maximkuzmin/checklister:${COMMIT}"
