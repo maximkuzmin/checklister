ca7f5ceaecf2b0101983ab777c5d0addd967313c
Max Kuzmin <mail@maximkuzmin.rocks>
2024-02-29 13:05:54 +0300

    Add DB creation if not created

--- lib/checklister/release.ex
+++ lib/checklister/release.ex
@@ -9,6 +9,7 @@ defmodule Checklister.Release do
     load_app()
 
     for repo <- repos() do
+      ensure_db_is_created!(repo)
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
   end
@@ -24,5 +25,15 @@ defmodule Checklister.Release do
 
   defp load_app do
     Application.load(@app)
+  end
+
+  defp ensure_db_is_created!(repo) do
+    IO.puts("create #{inspect(repo)} database if it doesn't exist")
+
+    case repo.__adapter__.storage_up(repo.config) do
+      :ok -> :ok
+      {:error, :already_up} -> :ok
+      {:error, term} -> {:error, term}
+    end
   end
 end
