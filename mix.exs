e338a028efb329d46be6fb0a269c788304062b7d
Max Kuzmin <mail@maximkuzmin.rocks>
2024-03-07 15:25:22 +0300

    Expand executable for the app

--- mix.exs
+++ mix.exs
@@ -9,7 +9,8 @@ defmodule Checklister.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      include_executables_for: [:unix]
     ]
   end
 
